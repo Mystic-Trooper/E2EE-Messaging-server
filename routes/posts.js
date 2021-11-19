346106b0b440f59764d28f82a467887d832c446b
Amit Patil <amitgpatil215@gmail.com>
2021-11-19 20:51:23 +0530

    deleting unused directories

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const verifyToken = require("./verifyToken");
-
-router.get("/", verifyToken, (req, res) => {
-  res.status(200).send(req.user);
-  // you have an acess to user you can find in databse and perform various tasks
-});
-
-module.exports = router;
